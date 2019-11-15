04e7af1c1eac033f36c07f3eb6aef036b75a7235
Yaron Zhao <yifang.zhao.95@gmail.com>
2019-11-15 14:15:01 -0500

    Created a Header component

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../node_modules/bootstrap/dist/css/bootstrap.css'
+import './app.css'
+import Header from './Header'
+
+const App = () =>
+    <Header/>;
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () =>
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4">TodoList</h1>
+            <p className="lead">
+                This is a example of using React and Redux to implement a simple TodoList
+            </p>
+        </div>
+    </div>;
+
+export default Header
