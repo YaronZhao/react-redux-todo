39210e758013e2e40baf38ca65bd3c08de020032
Yaron Zhao <yifang.zhao.95@gmail.com>
2019-11-15 16:06:38 -0500

    Initial set-up for redux

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addTodo} from "../actions";
 
-const AddTodo = () =>
-    <div className='input-group w-50'>
-        <input type="text" className="form-control" placeholder="What needs to be done?"/>
-        <div className="input-group-append">
-            <button className="btn btn-outline-secondary" type="button">Add Todo</button>
+const AddTodo = ({dispatch}) => {
+    let textInput = React.createRef();
+
+    return (
+        <div className='input-group w-50'>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="What needs to be done?"
+                ref={textInput}/>
+            <div className="input-group-append">
+                <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    onClick={() => dispatch(addTodo(textInput.current.value()))}>
+                    Add Todo
+                </button>
+            </div>
         </div>
-    </div>;
+    )
+};
 
-export default AddTodo
+export default connect()(AddTodo)
