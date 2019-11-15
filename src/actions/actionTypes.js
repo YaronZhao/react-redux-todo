4e95b1adbd647a535df3e9db6b804a2366d497d6
Yaron Zhao <yifang.zhao.95@gmail.com>
2019-11-15 15:34:01 -0500

    Created actionTypes

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,5 @@
+export const FETCH_TODO = "FETCH_TODO";
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
+export const SET_VISIBILITY_FILTER = "SET_VISBILITY_FILTER";
