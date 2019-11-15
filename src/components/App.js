3ba80827bf981d7cbbfcb4b5c5cca71045c8586b
Yaron Zhao <yifang.zhao.95@gmail.com>
2019-11-15 15:18:02 -0500

    Created a TodoListItem component

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 import './app.css'
 import Header from './Header'
 import AddTodo from '../containers/AddTodo'
+import TodoListItem from './TodoListItem'
 
 const App = () =>
     <div>
         <Header/>
         <div className="container">
             <AddTodo/>
+            <TodoListItem/>
         </div>
     </div>;
 
---
+++ src/components/TodoListItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoListItem = () =>
+    <div className="form-check pt-3">
+        <input className="form-check-input" type="checkbox"/>
+        <label className="form-check-label ml-2">Default checkbox</label>
+    </div>;
+
+export default TodoListItem
